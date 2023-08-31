eda28e4524f91b4e4b825325fe62593fa1cf56d2
townwish4git <tanweixi1@huawei.com>
2023-08-31 17:36:18 +0800

    test

--- examples/twx_examples/test.py
+++ examples/twx_examples/test.py
@@ -1,31 +1,49 @@
 import mindspore as ms
-import mindcv
-import mindspore.dataset as ds
+import numpy as np
+from mindspore import Tensor, nn, load_checkpoint, ops
+import matplotlib.pyplot as plt
 
-from mindspore import Tensor, nn
+img_size = 28  # 训练图像长（宽）
+latent_size = 100  # 隐码的长度
+class Generator(nn.Cell):
+    def __init__(self, latent_size, auto_prefix=True):
+        super(Generator, self).__init__(auto_prefix=auto_prefix)
+        self.model = nn.SequentialCell()
+        # [N, 100] -> [N, 128]
+        # 输入一个100维的0～1之间的高斯分布，然后通过第一层线性变换将其映射到256维
+        self.model.append(nn.Dense(latent_size, 128))
+        self.model.append(nn.ReLU())
+        # [N, 128] -> [N, 256]
+        self.model.append(nn.Dense(128, 256))
+        self.model.append(nn.BatchNorm1d(256))
+        self.model.append(nn.ReLU())
+        # [N, 256] -> [N, 512]
+        self.model.append(nn.Dense(256, 512))
+        self.model.append(nn.BatchNorm1d(512))
+        self.model.append(nn.ReLU())
+        # [N, 512] -> [N, 1024]
+        self.model.append(nn.Dense(512, 1024))
+        self.model.append(nn.BatchNorm1d(1024))
+        self.model.append(nn.ReLU())
+        # [N, 1024] -> [N, 784]
+        # 经过线性变换将其变成784维
+        self.model.append(nn.Dense(1024, img_size * img_size))
+        # 经过Tanh激活函数是希望生成的假的图片数据分布能够在-1～1之间
+        self.model.append(nn.Tanh())
 
-def valid(dataloader):
-    model.set_train(False)
-    correct_num = 0.
-    total_num = 0.
+    def construct(self, x):
+        img = self.model(x)
+        return ops.reshape(img, (-1, 1, 28, 28))
 
-    for data, label in dataloader:
-        data = Tensor(data, dtype=ms.float32).resize((batch_size, 1, 28, 28))
-        label = Tensor(label, dtype=ms.int32)
-        logits = model(data)
-        pred = logits.argmax(axis=1)
-        correct_num += (pred == label).sum().asnumpy()
-        total_num += label.shape[0]
+net_g = Generator(latent_size)
+load_checkpoint('./result/checkpoints/Generator199.ckpt', net_g)
 
-    return correct_num / total_num
-
-
-model = mindcv.LeNet(in_channels=1)
-ms.load_checkpoint(r"D:\Code\model_ckpt\LeNet5.ckpt", model)
-
-batch_size = 512
-dataset = ds.MnistDataset(r"D:\Code\Dataset\mnist", usage="test")
-dataset = dataset.batch(batch_size, drop_remainder=True)
-dataloader = dataset.create_tuple_iterator()
-
-print(valid(dataloader))
+test_data = Tensor(np.random.normal(0, 1, (25, 100)).astype(np.float32))
+images = net_g(test_data).transpose(0, 2, 3, 1).asnumpy()
+# 结果展示
+fig = plt.figure(figsize=(3, 3), dpi=120)
+for i in range(25):
+    fig.add_subplot(5, 5, i + 1)
+    plt.axis("off")
+    plt.imshow(images[i].squeeze(), cmap="gray")
+plt.show()
